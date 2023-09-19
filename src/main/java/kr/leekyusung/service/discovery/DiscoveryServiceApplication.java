ac74048ef9b793084f2b0b218e5c7761c244b3f9
LeeKyuSung <ks5050577@gmail.com>
2023-09-19 13:24:15 +0900

    aws 최종 세팅
    Update deploy-aws.yml
    
    Update DiscoveryServiceApplication.java
    
    Update DiscoveryServiceApplication.java

--- src/main/java/kr/leekyusung/service/discovery/DiscoveryServiceApplication.java
+++ src/main/java/kr/leekyusung/service/discovery/DiscoveryServiceApplication.java
@@ -2,7 +2,14 @@ package kr.leekyusung.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +19,19 @@ public class DiscoveryServiceApplication {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
+    @Bean
+    @Profile("prod")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        config.setNonSecurePort(80);
+        return config;
+    }
 }
