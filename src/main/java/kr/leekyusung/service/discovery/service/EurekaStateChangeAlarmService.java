53877d48d66d4f41e6ab8f1d94acb5bc89af805a
LeeKyuSung <ks5050577@gmail.com>
2023-09-27 01:54:33 +0900

    Update EurekaStateChangeAlarmService.java

--- src/main/java/kr/leekyusung/service/discovery/service/EurekaStateChangeAlarmService.java
+++ src/main/java/kr/leekyusung/service/discovery/service/EurekaStateChangeAlarmService.java
@@ -2,11 +2,13 @@ package kr.leekyusung.service.discovery.service;
 
 import kr.leekyusung.service.discovery.client.AlarmServiceClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EurekaStateChangeAlarmService {
@@ -17,23 +19,31 @@ public class EurekaStateChangeAlarmService {
     public void register(String appName, String instanceId, String hostName, int port) {
         if (!serverStatusMap.containsKey(instanceId)) {
             serverStatusMap.put(instanceId, hostName);
-            alarmServiceClient.sendAlarm(
-                    "DISCOVERY-SERVICE",
-                    appName + "\n"
-                            + "REGISTERED\n"
-                            + instanceId + "\n"
-                            + hostName + ":" + port);
+            try {
+                alarmServiceClient.sendAlarm(
+                        "DISCOVERY-SERVICE",
+                        appName + "\n"
+                                + "REGISTERED\n"
+                                + instanceId + "\n"
+                                + hostName + ":" + port);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
         }
     }
 
     public void down(String appName, String serverId) {
         if (serverStatusMap.containsKey(serverId)) {
             serverStatusMap.remove(serverId);
-            alarmServiceClient.sendAlarm(
-                    "DISCOVERY-SERVICE",
-                    appName + "\n"
-                            + "DOWN\n"
-                            + serverId);
+            try {
+                alarmServiceClient.sendAlarm(
+                        "DISCOVERY-SERVICE",
+                        appName + "\n"
+                                + "DOWN\n"
+                                + serverId);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
         }
     }
 
@@ -41,12 +51,16 @@ public class EurekaStateChangeAlarmService {
         if (serverStatusMap.containsKey(serverId)
                 && !serverStatusMap.get(serverId).equals(hostName)) {
             serverStatusMap.put(serverId, hostName);
-            alarmServiceClient.sendAlarm(
-                    "DISCOVERY-SERVICE",
-                    appName + "\n"
-                            + "RENEW\n"
-                            + serverId + "\n"
-                            + hostName + ":" + port);
+            try {
+                alarmServiceClient.sendAlarm(
+                        "DISCOVERY-SERVICE",
+                        appName + "\n"
+                                + "RENEW\n"
+                                + serverId + "\n"
+                                + hostName + ":" + port);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
         }
     }
 }
